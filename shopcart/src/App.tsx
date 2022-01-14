b017f8ad517a33d2dd9b3e06801428cfd0b42e7d
Callum <callum.taylor.reid@gmail.com>
2022-01-14 13:53:03 -0800

    Map through items returned from API, integrate Item component

--- shopcart/src/App.tsx
+++ shopcart/src/App.tsx
@@ -1,11 +1,12 @@
 import {useState} from 'react';
 import {useQuery} from 'react-query';
 
-// material ui components
+// components
 import {Drawer} from '@material-ui/core';
 import { LinearProgress } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 import { Badge } from '@material-ui/core';
+import Item from './Item/Item';
 
 // styles
 import {Wrapper} from './App.styles';
@@ -29,16 +30,22 @@ const getProducts = async(): Promise<CartItemType[]> =>
 const App = () => {
   const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
   const getTotalItems = () => null;
-  const handleAddToCart = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div> 'err errr errrrrr </div>;
 
   return (
-    <div className="App">
-        <p>mooooo</p>
-    </div>
+    <Wrapper>
+      <Grid container space={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
