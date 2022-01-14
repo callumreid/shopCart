43802cc99494a844072911587231ca0da4435f89
Callum <callum.taylor.reid@gmail.com>
2022-01-14 13:31:04 -0800

    Add fetch to API; create item component

--- shopcart/src/App.tsx
+++ shopcart/src/App.tsx
@@ -1,11 +1,43 @@
-import './App.css';
+import {useState} from 'react';
+import {useQuery} from 'react-query';
+
+// material ui components
+import {Drawer} from '@material-ui/core';
+import { LinearProgress } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { Badge } from '@material-ui/core';
+
+// styles
+import {Wrapper} from './App.styles';
+
+// types
+export type CartItemType = {
+  id: number;
+  cateogry: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+
+const getProducts = async(): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 
 const App = () => {
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+  const getTotalItems = () => null;
+  const handleAddToCart = () => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div> 'err errr errrrrr </div>;
+
   return (
     <div className="App">
-      <header className="App-header">
         <p>mooooo</p>
-      </header>
     </div>
   );
 }
--- shopcart/src/index.tsx
+++ shopcart/src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {QueryClient, QueryClientProvider} from 'react-query';
 import App from './App';
 
+const client = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </QueryClientProvider>,
   document.getElementById('root')
 );
 
